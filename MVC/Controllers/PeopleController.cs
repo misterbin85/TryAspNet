a6978944f84b179821701d0b5ea1dfaeb5709a0e
Oleh Matkovskyi <oleg.matkovskyi@gmail.com>
2018-11-27 18:37:49 +0200

    Added MVC part

---
+++ MVC/Controllers/PeopleController.cs
@@ -0,0 +1,27 @@
+﻿using MyTestMVC.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MyTestMVC.Controllers
+{
+	public class PeopleController : Controller
+	{
+		// GET: People
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult ListOfPeople()
+		{
+			List<PersonModel> personModels = new List<PersonModel>
+			{
+				new PersonModel{FirstName = "Name_1", LastName = "LastName_1"},
+				new PersonModel{FirstName = "Name_2", LastName = "LastName_2"},
+				new PersonModel{FirstName = "Name_3", LastName = "LastName_3"}
+			};
+
+			return View(personModels);
+		}
+	}
+}
---
+++ MVC/Models/PersonModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyTestMVC.Models
+{
+	public class PersonModel
+	{
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public int Age { get; set; } = 12;
+
+		public bool IsAlive { get; set; } = true;
+	}
+}
