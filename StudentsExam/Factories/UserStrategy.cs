e368d60fc72281de9c8c7e4ec0d8a4663df57757
Oleh Matkovskyi <oleg.matkovskyi@gmail.com>
2018-12-12 17:25:28 +0200

    Updated Factory to Strategy pattern

---
+++ StudentsExam/Factories/UserStrategy.cs
@@ -0,0 +1,30 @@
+﻿using StudentsExam.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentsExam.Factories
+{
+	public class UserStrategy
+	{
+		public static IUser RetrieveUser(int option) => (IUser)Activator.CreateInstance(AvailableUserTypes()[option]);
+
+		public static Dictionary<int, Type> AvailableUserTypes()
+		{
+			List<Type> entities = GetAllEntities();
+			var users = new Dictionary<int, Type>();
+
+			for (int i = 0; i < entities.Count; i++)
+			{
+				var loc = i;
+				users.Add(loc, entities[loc]);
+			}
+
+			return users;
+		}
+
+		private static List<Type> GetAllEntities() => AppDomain.CurrentDomain.GetAssemblies()
+			.SelectMany(assembly => assembly.GetTypes())
+			.Where(type => typeof(IUser).IsAssignableFrom(type) && !type.IsInterface).ToList();
+	}
+}
--- StudentsExam/Helpers/AuthenticationHelper.cs
+++ StudentsExam/Helpers/AuthenticationHelper.cs
@@ -6,86 +6,94 @@ using System.Linq;
 
 namespace StudentsExam.Helpers
 {
-    public class AuthenticationHelper
-    {
-        private delegate void ClearScreen();
+	public class AuthenticationHelper
+	{
+		private delegate void ClearScreen();
 
-        private delegate string ReadInput();
+		private delegate string ReadInput();
 
-        #region Fields
+		#region Fields
 
-        private readonly Users _allowedUsers;
+		private readonly Users _allowedUsers;
 
-        private readonly string _nl = Environment.NewLine;
+		private readonly string _nl = Environment.NewLine;
 
-        private readonly ReadInput _readInput = Console.ReadLine;
+		private readonly ReadInput _readInput = Console.ReadLine;
 
-        private readonly ClearScreen _clear = Console.Clear;
+		private readonly ClearScreen _clear = Console.Clear;
 
-        #endregion Fields
+		#endregion Fields
 
-        #region Methods
+		#region Methods
 
-        public AuthenticationHelper()
-        {
-            _allowedUsers = new Users(ConfigurationManager.AppSettings["LoginUsers"]);
-        }
+		public AuthenticationHelper()
+		{
+			_allowedUsers = new Users(ConfigurationManager.AppSettings["LoginUsers"]);
+		}
 
-        public IUser Authenticate()
-        {
-            IUser user = SelectUserType();
-            user = ObtainCredentials(user);
+		public IUser Authenticate()
+		{
+			IUser user = SelectUserType();
+			user = ObtainCredentials(user);
 
-            if (user.GetType() == typeof(Student))
-            {
-                user = _allowedUsers.Students.FirstOrDefault(student => student.Name.ToLower().Equals(user.Name) && student.Email.ToLower().Equals(user.Email));
-            }
-            else
-            {
-                user = _allowedUsers.Teachers.FirstOrDefault(teacher => teacher.Name.ToLower().Equals(user.Name) && teacher.Email.ToLower().Equals(user.Email));
-            }
+			if (user.GetType() == typeof(Student))
+			{
+				user = _allowedUsers.Students.FirstOrDefault(student => student.Name.ToLower().Equals(user.Name) && student.Email.ToLower().Equals(user.Email));
+			}
+			else
+			{
+				user = _allowedUsers.Teachers.FirstOrDefault(teacher => teacher.Name.ToLower().Equals(user.Name) && teacher.Email.ToLower().Equals(user.Email));
+			}
 
-            if (user == null)
-            {
-                _clear();
-                Console.WriteLine("Selected User wasn't found. Please try again");
-                return Authenticate();
-            }
+			if (user == null)
+			{
+				_clear();
+				Console.WriteLine("Selected User wasn't found. Please try again");
+				return Authenticate();
+			}
 
-            return user;
-        }
+			return user;
+		}
 
-        private IUser SelectUserType()
-        {
-            Console.WriteLine($"Please select:{_nl} 1 - you are a Teacher {_nl} 2 - you are a Student{_nl} And hit Enter...");
+		private IUser SelectUserType()
+		{
+			var availableUserTypes = UserStrategy.AvailableUserTypes();
 
-            string userInput = _readInput();
+			Console.WriteLine($"Available options are:");
 
-            bool res = int.TryParse(userInput, out int choice);
+			foreach (var aKey in availableUserTypes.Keys)
+			{
+				Console.WriteLine($"{aKey} - {availableUserTypes[aKey].Name}");
+			}
+			Console.WriteLine($"Please select your choice:{_nl}  And hit Enter...");
 
-            if (!res || choice == 0 || choice > 2)
-            {
-                _clear();
-                Console.WriteLine("Please select only mentioned numbers. Trying again:");
-                return SelectUserType();
-            }
+			string userInput = _readInput();
 
-            _clear();
-            return UsersFactory.GetUser(choice);
-        }
+			bool res = int.TryParse(userInput, out int choice);
 
-        private IUser ObtainCredentials(IUser user)
-        {
-            Console.WriteLine($"You're trying to login as a:'{user.GetType().Name}'.");
-            Console.WriteLine("Please enter your Name:");
-            user.Name = _readInput()?.ToLower();
-            Console.WriteLine("Please enter your Email:");
-            user.Email = _readInput().ToLower();
-            _clear();
+			if (!res || choice > availableUserTypes.Count)
+			{
+				_clear();
+				Console.WriteLine("Please select only mentioned numbers. Trying again:");
+				return SelectUserType();
+			}
 
-            return user;
-        }
+			_clear();
+			return UserStrategy.RetrieveUser(choice);
+		}
 
-        #endregion Methods
-    }
+		private IUser ObtainCredentials(IUser user)
+		{
+			Console.WriteLine($"You're trying to login as a:'{user.GetType().Name}'.");
+			Console.WriteLine("Please enter your Name:");
+			user.Name = _readInput()?.ToLower();
+			Console.WriteLine("Please enter your Email:");
+			user.Email = _readInput().ToLower();
+			_clear();
+
+			return user;
+		}
+
+		#endregion Methods
+	}
 }
