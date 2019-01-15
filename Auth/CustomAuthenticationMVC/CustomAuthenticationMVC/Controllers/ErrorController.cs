fb777ed2244b1e443826c51bb20568e17025324e
Oleh Matkovskyi <oleg.matkovskyi@gmail.com>
2019-01-15 12:42:47 +0200

    Minor code cleanup

--- Auth/CustomAuthenticationMVC/CustomAuthenticationMVC/Controllers/ErrorController.cs
+++ Auth/CustomAuthenticationMVC/CustomAuthenticationMVC/Controllers/ErrorController.cs
@@ -1,22 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace CustomAuthenticationMVC.Controllers
 {
-    public class ErrorController : Controller
-    {
-        // GET: Error
-        public ActionResult AccessDenied()
-        {
-            return View();
-        }
+	public class ErrorController : Controller
+	{
+		// GET: Error
+		public ActionResult AccessDenied()
+		{
+			return View();
+		}
 
-        public ActionResult PageNotFound()
-        {
-            return View("PageNotFound");
-        }
-    }
+		public ActionResult PageNotFound()
+		{
+			return View();
+		}
+
+		public ActionResult AnyError()
+		{
+			return View();
+		}
+	}
 }
--- Auth/CustomAuthenticationMVC/CustomAuthenticationMVC/Global.asax.cs
+++ Auth/CustomAuthenticationMVC/CustomAuthenticationMVC/Global.asax.cs
@@ -2,7 +2,6 @@
 using CustomAuthenticationMVC.Models;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,33 +10,32 @@ using System.Web.Security;
 
 namespace CustomAuthenticationMVC
 {
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-        }
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+		}
 
-        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
-        {
-            HttpCookie authCookie = Request.Cookies["Cookie1"];
-            if (authCookie != null)
-            {
-                FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+		protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+		{
+			HttpCookie authCookie = Request.Cookies["Cookie1"];
+			if (authCookie != null)
+			{
+				FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
 
-                var serializeModel = JsonConvert.DeserializeObject<CustomSerializeModel>(authTicket.UserData);
+				var serializeModel = JsonConvert.DeserializeObject<CustomSerializeModel>(authTicket.UserData);
 
-                CustomPrincipal principal = new CustomPrincipal(authTicket.Name);
+				CustomPrincipal principal = new CustomPrincipal(authTicket.Name);
 
-                principal.UserId = serializeModel.UserId;
-                principal.FirstName = serializeModel.FirstName;
-                principal.LastName = serializeModel.LastName;
-                principal.Roles = serializeModel.RoleName.ToArray<string>();
+				principal.UserId = serializeModel.UserId;
+				principal.FirstName = serializeModel.FirstName;
+				principal.LastName = serializeModel.LastName;
+				principal.Roles = serializeModel.RoleName.ToArray<string>();
 
-                HttpContext.Current.User = principal;
-            }
-
-        }
-    }
+				HttpContext.Current.User = principal;
+			}
+		}
+	}
 }
