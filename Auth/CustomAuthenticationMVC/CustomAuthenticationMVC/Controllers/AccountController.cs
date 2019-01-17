46d4b57c3f8a489daff7bb065c0619f1dc57c27f
Oleh Matkovskyi <omatkovskyi@nedelta.com>
2019-01-18 00:19:49 +0200

    Nothing really

--- Auth/CustomAuthenticationMVC/CustomAuthenticationMVC/Controllers/AccountController.cs
+++ Auth/CustomAuthenticationMVC/CustomAuthenticationMVC/Controllers/AccountController.cs
@@ -13,200 +13,199 @@ using System.Web.Security;
 
 namespace CustomAuthenticationMVC.Controllers
 {
-	[AllowAnonymous]
-	public class AccountController : Controller
-	{
-		// GET: Account
-		public ActionResult Index()
-		{
-			return View();
-		}
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-		[HttpGet]
-		public ActionResult Login(string ReturnUrl = "")
-		{
-			if (User.Identity.IsAuthenticated)
-			{
-				return LogOut();
-			}
-			ViewBag.ReturnUrl = ReturnUrl;
-			return View();
-		}
+        [HttpGet]
+        public ActionResult Login(string ReturnUrl = "")
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return LogOut();
+            }
+            ViewBag.ReturnUrl = ReturnUrl;
+            return View();
+        }
 
-		[HttpPost]
-		public ActionResult Login(LoginView loginView, string ReturnUrl = "")
-		{
-			if (ModelState.IsValid)
-			{
-				if (Membership.ValidateUser(loginView.UserName, loginView.Password))
-				{
-					var user = (CustomMembershipUser)Membership.GetUser(loginView.UserName, false);
-					if (user != null)
-					{
-						CustomSerializeModel userModel = new Models.CustomSerializeModel()
-						{
-							UserId = user.UserId,
-							FirstName = user.FirstName,
-							LastName = user.LastName,
-							RoleName = user.Roles.Select(r => r.RoleName).ToList()
-						};
+        [HttpPost]
+        public ActionResult Login(LoginView loginView, string ReturnUrl = "")
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(loginView.UserName, loginView.Password))
+                {
+                    var user = (CustomMembershipUser)Membership.GetUser(loginView.UserName, false);
+                    if (user != null)
+                    {
+                        CustomSerializeModel userModel = new CustomSerializeModel()
+                        {
+                            UserId = user.UserId,
+                            FirstName = user.FirstName,
+                            LastName = user.LastName,
+                            RoleName = user.Roles.Select(r => r.RoleName).ToList()
+                        };
 
-						string userData = JsonConvert.SerializeObject(userModel);
+                        string userData = JsonConvert.SerializeObject(userModel);
 
-						FormsAuthenticationTicket authTicket = new FormsAuthenticationTicket(
-								1, loginView.UserName, DateTime.Now, DateTime.Now.AddMinutes(15), false, userData);
+                        FormsAuthenticationTicket authTicket = new FormsAuthenticationTicket(1, loginView.UserName, DateTime.Now, DateTime.Now.AddMinutes(15), false, userData);
 
-						string enTicket = FormsAuthentication.Encrypt(authTicket);
-						HttpCookie faCookie = new HttpCookie("Cookie1", enTicket);
-						Response.Cookies.Add(faCookie);
-					}
+                        string enTicket = FormsAuthentication.Encrypt(authTicket);
+                        HttpCookie faCookie = new HttpCookie("Cookie1", enTicket);
+                        Response.Cookies.Add(faCookie);
+                    }
 
-					if (Url.IsLocalUrl(ReturnUrl))
-					{
-						return Redirect(ReturnUrl);
-					}
-					else
-					{
-						return RedirectToAction("Index");
-					}
-				}
-			}
-			ModelState.AddModelError("", "Something Wrong : Username or Password invalid ^_^ ");
-			return View(loginView);
-		}
+                    if (Url.IsLocalUrl(ReturnUrl))
+                    {
+                        return Redirect(ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Something Wrong : Username or Password invalid ^_^ ");
+            return View(loginView);
+        }
 
-		[HttpGet]
-		public ActionResult Registration()
-		{
-			var view = new RegistrationView();
-			using (AuthenticationDB db = new AuthenticationDB())
-			{
-				view.Roles = db.Roles.ToList();
-			}
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            var view = new RegistrationView();
+            using (AuthenticationDB db = new AuthenticationDB())
+            {
+                view.Roles = db.Roles.ToList();
+            }
 
-			return View(view);
-		}
+            return View(view);
+        }
 
-		[HttpPost]
-		public ActionResult Registration(RegistrationView registrationView)
-		{
-			bool statusRegistration = false;
-			string messageRegistration = string.Empty;
+        [HttpPost]
+        public ActionResult Registration(RegistrationView registrationView)
+        {
+            bool statusRegistration = false;
+            string messageRegistration = string.Empty;
 
-			if (ModelState.IsValid)
-			{
-				// Email Verification
-				string userName = Membership.GetUserNameByEmail(registrationView.Email);
-				if (!string.IsNullOrEmpty(userName))
-				{
-					ModelState.AddModelError("Warning Email", @"Sorry: Email already Exists");
-					return View(registrationView);
-				}
+            if (ModelState.IsValid)
+            {
+                // Email Verification
+                string userName = Membership.GetUserNameByEmail(registrationView.Email);
+                if (!string.IsNullOrEmpty(userName))
+                {
+                    ModelState.AddModelError("Warning Email", @"Sorry: Email already Exists");
+                    return View(registrationView);
+                }
 
-				//Save User Data
-				using (AuthenticationDB dbContext = new AuthenticationDB())
-				{
-					var user = new User()
-					{
-						Username = registrationView.Username,
-						FirstName = registrationView.FirstName,
-						LastName = registrationView.LastName,
-						Email = registrationView.Email,
-						Password = registrationView.Password,
-						ActivationCode = Guid.NewGuid(),
-						Roles = new List<Role> { dbContext.Roles.FirstOrDefault(role => role.RoleName.Equals(registrationView.Role)) }
-					};
+                //Save User Data
+                using (AuthenticationDB dbContext = new AuthenticationDB())
+                {
+                    var user = new User()
+                    {
+                        Username = registrationView.Username,
+                        FirstName = registrationView.FirstName,
+                        LastName = registrationView.LastName,
+                        Email = registrationView.Email,
+                        Password = registrationView.Password,
+                        ActivationCode = Guid.NewGuid(),
+                        Roles = new List<Role> { dbContext.Roles.FirstOrDefault(role => role.RoleName.Equals(registrationView.Role)) }
+                    };
 
-					dbContext.Users.Add(user);
-					dbContext.SaveChanges();
-				}
+                    dbContext.Users.Add(user);
+                    dbContext.SaveChanges();
+                }
 
-				//Verification Email
-				VerificationEmail(registrationView.Email, registrationView.ActivationCode.ToString());
-				messageRegistration = "Your account has been created successfully. ^_^";
-				statusRegistration = true;
-			}
-			else
-			{
-				messageRegistration = "Something Wrong!";
-			}
-			ViewBag.Message = messageRegistration;
-			ViewBag.Status = statusRegistration;
+                //Verification Email
+                //VerificationEmail(registrationView.Email, registrationView.ActivationCode.ToString());
+                messageRegistration = "Your account has been created successfully. ^_^";
+                statusRegistration = true;
+            }
+            else
+            {
+                messageRegistration = "Something Wrong!";
+            }
+            ViewBag.Message = messageRegistration;
+            ViewBag.Status = statusRegistration;
 
-			return View(registrationView);
-		}
+            return View(registrationView);
+        }
 
-		[HttpGet]
-		public ActionResult ActivationAccount(string id)
-		{
-			bool statusAccount = false;
-			using (AuthenticationDB dbContext = new AuthenticationDB())
-			{
-				var userAccount = dbContext.Users.FirstOrDefault(u => u.ActivationCode.ToString().Equals(id));
+        [HttpGet]
+        public ActionResult ActivationAccount(string id)
+        {
+            bool statusAccount = false;
+            using (AuthenticationDB dbContext = new AuthenticationDB())
+            {
+                var userAccount = dbContext.Users.FirstOrDefault(u => u.ActivationCode.ToString().Equals(id));
 
-				if (userAccount != null)
-				{
-					userAccount.IsActive = true;
-					dbContext.SaveChanges();
-					statusAccount = true;
-				}
-				else
-				{
-					ViewBag.Message = "Something Wrong !!";
-				}
-			}
-			ViewBag.Status = statusAccount;
-			return View();
-		}
+                if (userAccount != null)
+                {
+                    userAccount.IsActive = true;
+                    dbContext.SaveChanges();
+                    statusAccount = true;
+                }
+                else
+                {
+                    ViewBag.Message = "Something Wrong !!";
+                }
+            }
+            ViewBag.Status = statusAccount;
+            return View();
+        }
 
-		public ActionResult LogOut()
-		{
-			HttpCookie cookie = new HttpCookie("Cookie1", "");
-			cookie.Expires = DateTime.Now.AddYears(-1);
-			Response.Cookies.Add(cookie);
+        public ActionResult LogOut()
+        {
+            HttpCookie cookie = new HttpCookie("Cookie1", "");
+            cookie.Expires = DateTime.Now.AddYears(-1);
+            Response.Cookies.Add(cookie);
 
-			FormsAuthentication.SignOut();
-			return RedirectToAction("Login", "Account", null);
-		}
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account", null);
+        }
 
-		[NonAction]
-		public void VerificationEmail(string email, string activationCode)
-		{
-			var url = string.Format("/Account/ActivationAccount/{0}", activationCode);
-			var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, url);
+        [NonAction]
+        public void VerificationEmail(string email, string activationCode)
+        {
+            var url = string.Format("/Account/ActivationAccount/{0}", activationCode);
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, url);
 
-			var fromEmail = new MailAddress("mehdi.rami2012@gmail.com", "Activation Account - AKKA");
-			var toEmail = new MailAddress(email);
+            var fromEmail = new MailAddress("mehdi.rami2012@gmail.com", "Activation Account - AKKA");
+            var toEmail = new MailAddress(email);
 
-			var fromEmailPassword = "******************";
-			string subject = "Activation Account !";
+            var fromEmailPassword = "******************";
+            string subject = "Activation Account !";
 
-			string body = "<br/> Please click on the following link in order to activate your account" + "<br/><a href='" + link + "'> Activation Account ! </a>";
+            string body = "<br/> Please click on the following link in order to activate your account" + "<br/><a href='" + link + "'> Activation Account ! </a>";
 
-			var smtp = new SmtpClient
-			{
-				Host = "smtp.gmail.com",
-				Port = 587,
-				EnableSsl = true,
-				DeliveryMethod = SmtpDeliveryMethod.Network,
-				UseDefaultCredentials = false,
-				Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
-			};
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+            };
 
-			using (var message = new MailMessage(fromEmail, toEmail)
-			{
-				Subject = subject,
-				Body = body,
-				IsBodyHtml = true
-			})
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
 
-				smtp.Send(message);
-		}
+                smtp.Send(message);
+        }
 
-		protected override void OnException(ExceptionContext filterContext)
-		{
-			filterContext.ExceptionHandled = true;
-			filterContext.Result = RedirectToAction("AnyError", "Error");
-		}
-	}
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            filterContext.ExceptionHandled = true;
+            filterContext.Result = RedirectToAction("AnyError", "Error");
+        }
+    }
 }
--- StudentsExam/Entities/Users.cs
+++ StudentsExam/Entities/Users.cs
@@ -1,25 +1,24 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Newtonsoft.Json.Linq;
 using StudentsExam.FileUtils;
 
 namespace StudentsExam.Entities
 {
-	public class Users
-	{
-		public Users(string fileName)
-		{
-			this.LoadLoadUsers(JsonUtils.LoadJsonFile(fileName));
-		}
+    public class Users
+    {
+        public Users(string fileName)
+        {
+            this.LoadLoadUsers(JsonUtils.LoadJsonFile(fileName));
+        }
 
-		private void LoadLoadUsers(JObject o)
-		{
-			this.Teachers = o["Users"].First["Teachers"].ToObject<IList<Teacher>>();
-			this.Students = o["Users"].Last["Students"].ToObject<IList<Student>>();
-		}
+        private void LoadLoadUsers(JObject o)
+        {
+            this.Teachers = o["Users"].First["Teachers"].ToObject<IList<Teacher>>();
+            this.Students = o["Users"].Last["Students"].ToObject<IList<Student>>();
+        }
 
-		public IList<Student> Students { get; set; }
+        public IList<Student> Students { get; set; }
 
-		public IList<Teacher> Teachers { get; set; }
-	}
+        public IList<Teacher> Teachers { get; set; }
+    }
 }
