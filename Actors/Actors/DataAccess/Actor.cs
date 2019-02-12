820fc356c47c7ce57b0eabc5ce26f12a882c53d6
Oleh <oleg.matkovskyi@gmail.com>
2019-02-12 23:21:56 +0200

    Added DB context

---
+++ Actors/Actors/DataAccess/Actor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Actors.DataAccess
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Bio { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+    }
+}
---
+++ Actors/Actors/DataAccess/Photo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Actors.DataAccess
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+
+        public string Path { get; set; }
+
+        public int ActorId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(ActorId))]
+        public Actor Actor { get; set; }
+    }
+}
