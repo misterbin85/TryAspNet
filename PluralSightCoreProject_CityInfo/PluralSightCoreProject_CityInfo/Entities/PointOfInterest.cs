ac8dc3d351c5b156d0d0ea21cce018b8461cf95a
Oleh Matkovskyi <omatkovskyi@nedelta.com>
2019-10-15 01:05:39 +0300

    Added Entities

---
+++ PluralSightCoreProject_CityInfo/PluralSightCoreProject_CityInfo/Entities/PointOfInterest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Permissions;
+
+namespace PluralSightCoreProject_CityInfo.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [ForeignKey("CityId")]
+        public City City { get; set; }
+
+        public int CityId { get; set; }
+    }
+}
