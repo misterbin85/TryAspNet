669946bad0e19dd90c28bc66564ff6eddcb22bab
Oleh Matkovskyi <omatkovskyi@nedelta.com>
2019-12-15 03:22:33 +0200

    [AccessSoftek] Initial commit

---
+++ AccessSoftekCore/Configs/Interfaces/IApplication.cs
@@ -0,0 +1,13 @@
+﻿using Config.Net;
+
+namespace AccessSoftekCore.Configs.Interfaces
+{
+    public interface IApplication
+    {
+        [Option(Alias = "name")]
+        string Name { get; set; }
+
+        [Option(Alias = "url")]
+        IUrl Url { get; }
+    }
+}
